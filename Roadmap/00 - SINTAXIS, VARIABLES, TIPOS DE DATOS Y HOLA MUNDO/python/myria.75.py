fa7f1983aa76877a4bb2c86d657cbb95122686c6
myria <80063573+myria75@users.noreply.github.com>
2024-09-14 19:41:03 +0200

    00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/myria.75.py
@@ -0,0 +1,22 @@
+
+# https://www.python.org/
+
+# comentario de una linea
+
+"""
+    comentario de varias líneas
+"""
+
+x = 1
+
+PI = 3.1416
+
+booleano = True
+
+decimal = 0.666
+
+entero = 5
+
+cadena = "hola"
+
+print("¡Hola, python!")
